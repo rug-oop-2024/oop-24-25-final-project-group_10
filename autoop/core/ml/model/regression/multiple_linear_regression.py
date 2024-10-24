50655fe4391172502f46b8da0a4f9fba60145ce1
joelbenjaminboer <joelbenjaminboer@gmail.com>
2024-10-24 15:16:20 +0200

    Switched files around for regression still needs to be done for classification the same way. Test now runs but gives errors :(

---
+++ autoop/core/ml/model/regression/multiple_linear_regression.py
@@ -0,0 +1,31 @@
+from sklearn.linear_model import LinearRegression
+from autoop.core.ml.model.model import Model
+
+
+class MultipleLinearRegression(Model):
+    def __init__(self):
+        """
+        Initializes the wrapper with a
+        LinearRegression model from scikit-learn.
+        """
+        self._model = LinearRegression()
+
+    def fit(self, X, y):
+        """Fits the model to the training data.
+
+        Args:
+            X: Feature matrix (n_samples, n_features)
+            y: Target vector (n_samples,)
+        """
+        self._model.fit(X, y)
+
+    def predict(self, X):
+        """Predicts the target values for given features.
+
+        Args:
+            X: Feature matrix (n_samples, n_features)
+
+        Returns:
+            Predictions for the input data.
+        """
+        return self._model.predict(X)
